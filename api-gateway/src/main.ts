c8297d7a23a9fbed58e439aec335a460e45f8382
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-08 23:01:30 -0300

    feat: add timeout interceptor

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeoutInterceptor } from './shared/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
