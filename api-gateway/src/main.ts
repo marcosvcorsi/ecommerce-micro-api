c4ca6fa80b5555679defffddf0a9f79c0bcb4de9
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-08 23:10:10 -0300

    feat: add http exception filter

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { TimeoutInterceptor } from './shared/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
