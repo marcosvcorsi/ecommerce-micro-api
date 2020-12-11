3e20a90376f716dcbd6a7dc9439611910698bd58
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-10 22:48:48 -0300

    feat: add product module scaffold

--- micro-products/src/app.module.ts
+++ micro-products/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ProductsModule,
   ],
 })
 export class AppModule {}
