3b6c2e8622688bf68996992e6ecebb8435065053
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-10 23:18:53 -0300

    chore: add typeorm and configs

--- micro-products/src/app.module.ts
+++ micro-products/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      migrations: ['dist/shared/database/migrations/*.js'],
+      cli: {
+        migrationsDir: 'src/shared/database/migrations',
+      },
     }),
     ProductsModule,
   ],
