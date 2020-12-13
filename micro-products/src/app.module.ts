c5ffa2c36410adce693b53a693d9265a75581b31
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-12 22:43:16 -0300

    feat: add product migration and configs

--- micro-products/src/app.module.ts
+++ micro-products/src/app.module.ts
@@ -8,19 +8,7 @@ import { ProductsModule } from './products/products.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      migrations: ['dist/shared/database/migrations/*.js'],
-      cli: {
-        migrationsDir: 'src/shared/database/migrations',
-      },
-    }),
+    TypeOrmModule.forRoot(),
     ProductsModule,
   ],
 })
---
+++ micro-products/src/shared/database/migrations/1607823160136-CreateProducts.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProducts1607823160136 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.dropTable('products');
+  }
+}
