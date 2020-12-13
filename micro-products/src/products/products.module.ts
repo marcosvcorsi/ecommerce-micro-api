c6a3dcd043b1394a23eac716d69182b31c8ec791
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-13 00:39:34 -0300

    feat: add create es product index

--- micro-products/src/products/products.module.ts
+++ micro-products/src/products/products.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsRepository } from './products.repository';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductsRepository])],
+  imports: [
+    TypeOrmModule.forFeature([ProductsRepository]),
+    ElasticsearchModule.registerAsync({
+      useFactory: () => ({
+        node: process.env.ES_URL,
+      }),
+    }),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
--- micro-products/src/products/products.service.ts
+++ micro-products/src/products/products.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { Product } from './product.entity';
 import { ProductsRepository } from './products.repository';
 
 @Injectable()
 export class ProductsService {
-  constructor(private readonly productsRepository: ProductsRepository) {}
+  constructor(
+    private readonly productsRepository: ProductsRepository,
+    private readonly elasticsearchService: ElasticsearchService,
+  ) {}
 
   async findAll(): Promise<Product[]> {
     return this.productsRepository.find();
@@ -14,6 +18,16 @@ export class ProductsService {
     const product = this.productsRepository.create(productDto);
 
     await this.productsRepository.save(product);
+
+    const { id, name } = product;
+
+    await this.elasticsearchService.index({
+      index: 'products',
+      body: {
+        id,
+        name,
+      },
+    });
 
     return product;
   }
