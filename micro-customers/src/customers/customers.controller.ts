fd6effaa9b26120a08a73fa75b0b5e4a07c900ff
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-19 23:51:24 -0300

    feat: add create customer controller and micro service config

--- micro-customers/src/customers/customers.controller.ts
+++ micro-customers/src/customers/customers.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
 
 @Controller()
-export class CustomersController {}
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @MessagePattern('create-customer')
+  async create(@Payload() createCustomerDto: CreateCustomerDto) {
+    return this.customersService.create(createCustomerDto);
+  }
+}
