b34d2e12e75ec6bd5fd600423a8f391c92677ae2
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-20 00:24:27 -0300

    fix: salt error

--- micro-customers/src/shared/services/hash.service.ts
+++ micro-customers/src/shared/services/hash.service.ts
@@ -8,7 +8,11 @@ export class HashService {
   constructor(private readonly configService: ConfigService) {}
 
   async hash(value: string): Promise<string> {
-    return bcrypt.hash(value, this.configService.get<number>('HASH_SALT'));
+    const salt = await bcrypt.genSalt(
+      Number(this.configService.get<number>('HASH_SALT')),
+    );
+
+    return bcrypt.hash(value, salt);
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
