4337441356f105e875cc1151cd447bf19ca1f59a
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-20 21:55:37 -0300

    feat: add customer auth route

--- api-gateway/src/customers/customers.controller.ts
+++ api-gateway/src/customers/customers.controller.ts
@@ -1,22 +1,44 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ClientProxyService } from 'src/shared/services/client-proxy.service';
+import { AuthLoginDto } from './dtos/auth-login.dto';
 import { CreateCustomerDto } from './dtos/create-customer.dto';
 
 @Controller('customers')
 export class CustomersController {
-  private readonly clientProxyProduct: ClientProxy;
+  private readonly clientProxyCustomers: ClientProxy;
 
   constructor(clientProxyService: ClientProxyService) {
-    this.clientProxyProduct = clientProxyService.getClientProxyInstance(
+    this.clientProxyCustomers = clientProxyService.getClientProxyInstance(
       'customers',
     );
   }
 
   @Post('/register')
   async register(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.clientProxyProduct
+    return this.clientProxyCustomers
       .send('create-customer', createCustomerDto)
       .toPromise();
+  }
+
+  @Post('/auth')
+  @HttpCode(HttpStatus.OK)
+  async auth(@Body() authLoginDto: AuthLoginDto) {
+    const response = await this.clientProxyCustomers
+      .send('auth-customer', authLoginDto)
+      .toPromise();
+
+    if (response.error) {
+      throw new UnauthorizedException(response.error);
+    }
+
+    return response;
   }
 }
