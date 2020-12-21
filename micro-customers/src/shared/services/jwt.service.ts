8a9dd82ce38fcc92a63d983f4e966493b185a442
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-20 21:36:19 -0300

    feat: add jwt lib and config

---
+++ micro-customers/src/shared/services/jwt.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtService {
+  constructor(private readonly configService: ConfigService) {}
+
+  generate(payload: any) {
+    return jwt.sign(
+      payload,
+      this.configService.get<string>('JWT_PRIVATE_KEY'),
+      { algorithm: 'RS256' },
+    );
+  }
+}
