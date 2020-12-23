9f064488e4523648dae94fac51b5ef361b5c85a4
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-22 23:10:05 -0300

    chore: add mongoose module

--- micro-orders/src/app.module.ts
+++ micro-orders/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     }),
     OrdersModule,
   ],
