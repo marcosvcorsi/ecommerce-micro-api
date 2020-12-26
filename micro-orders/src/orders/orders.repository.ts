d3f0439de6504f15f89d2050d7ed2f454f793aa4
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-25 22:03:54 -0300

    fix: add date and status to order creation

--- micro-orders/src/orders/orders.repository.ts
+++ micro-orders/src/orders/orders.repository.ts
@@ -1,7 +1,7 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { Order, OrderDocument } from './order.schema';
+import { Order, OrderDocument, OrderStatus } from './order.schema';
 
 export class OrdersRepository {
   constructor(
@@ -10,6 +10,9 @@ export class OrdersRepository {
 
   async create(createOrderDto: CreateOrderDto) {
     const order = new this.orderModel(createOrderDto);
+
+    order.date = new Date();
+    order.status = OrderStatus.PROCESSING;
 
     await order.save();
 
